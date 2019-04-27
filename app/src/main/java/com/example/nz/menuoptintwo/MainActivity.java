36b3084f3ced43dab9c3abb33b29d622226d56a2
Shaid Azmin <shaidazmin@gmail.com>
2019-04-27 06:08:13 +0600

    git init

---
+++ app/src/main/java/com/example/nz/menuoptintwo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.nz.menuoptintwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // menu option .....
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== R.id.settings){
+            Toast.makeText(MainActivity.this,"Settingis",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
